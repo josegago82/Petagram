bfaaf7b27f1bf40402bf0efc04f3a560016b8b00
josegago82 <78693499+josegago82@users.noreply.github.com>
2021-02-12 17:24:46 +0100

    Initial commit

---
+++ app/src/main/java/com/josp/petagram/MainActivity.java
@@ -0,0 +1,21 @@
+package com.josp.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDefaultDisplayHomeAsUpEnabled(true);
+    }
+}
